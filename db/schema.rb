463c0a04c9509703ae2ae8b1c6c0d2b7f9f43bc8
Geoff Hull <geoffhull03@gmail.com>
2017-06-03 13:07:04 -0700

    Added Advertisement Page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170531031114) do
+ActiveRecord::Schema.define(version: 20170603052645) do
+
+  create_table "advertisements", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "answers", force: :cascade do |t|
     t.integer "question_id"
