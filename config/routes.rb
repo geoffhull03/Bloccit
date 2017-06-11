1b32e402c51261242d490806b8875251373babf6
Geoff Hull <geoffhull03@gmail.com>
2017-06-11 13:39:55 -0700

    Added sign-in authentication

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
    end
   resources :sponsored_post
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   get 'about' => 'welcome#about'
 
   get 'welcome/contact'
