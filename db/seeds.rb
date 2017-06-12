55ca1b69115a0a01b7df2f8f913c772ceffef3b5
Geoff Hull <geoffhull03@gmail.com>
2017-06-12 12:51:44 -0700

    Added user receipt on posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'random_data'
 
+5.times do
+   User.create!(
+ # #3
+   name:     RandomData.random_name,
+   email:    RandomData.random_email,
+   password: RandomData.random_sentence
+   )
+ end
+users = User.all
+
+
 15.times do
    Topic.create!(
      name:         RandomData.random_sentence,
@@ -17,6 +28,7 @@ require 'random_data'
 
  50.times do
    Post.create!(
+     user:   users.sample,
      topic:  topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
@@ -40,11 +52,18 @@ end
   )
 end
 
+user = User.first
+ user.update_attributes!(
+   email: 'youremail.com', # replace this with your personal email
+   password: 'helloworld'
+ )
+
 puts "#{Post.count}"
 Post.find_or_create_by(title: "My unique title", body: "A unique body")
 puts "#{Post.count}"
 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
--- lib/random_data.rb
+++ lib/random_data.rb
@@ -1,5 +1,14 @@
 module RandomData
- # #6
+  def self.random_name
+     first_name = random_word.capitalize
+     last_name = random_word.capitalize
+     "#{first_name} #{last_name}"
+  end
+
+   def self.random_email
+     "#{random_word}@#{random_word}.#{random_word}"
+   end
+
    def self.random_paragraph
      sentences = []
      rand(4..6).times do
@@ -9,7 +18,6 @@ module RandomData
      sentences.join(" ")
    end
 
- # #7
    def self.random_sentence
      strings = []
      rand(3..8).times do
@@ -26,4 +34,4 @@ module RandomData
      letters.shuffle!
      letters[0,rand(3..8)].join
    end
- end
+end
