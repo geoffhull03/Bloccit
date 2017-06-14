336779fc094839fb330a6f75411c51a683ad8a85
Geoff Hull <geoffhull03@gmail.com>
2017-06-13 22:20:39 -0700

    Added Comments ability

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   resources :sponsored_post
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :posts, only: [] do
+
+     resources :comments, only: [:create, :destroy]
+   end
   get 'about' => 'welcome#about'
 
   get 'welcome/contact'
